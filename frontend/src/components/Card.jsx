d3127a81fb454623f4fdd281c6685fc6b13d1312
aryanmishra64 <aryanthechamp64@gmail.com>
2024-06-16 23:01:57 +0530

    Connected both client and server with all necessary functionalities

--- frontend/src/components/Card.jsx
+++ frontend/src/components/Card.jsx
@@ -16,7 +16,7 @@ const categoryColorMap = {
 	// Add more categories and corresponding color classes as needed
 };
 
-const Card = ({ transaction }) => {
+const Card = ({ transaction, authUser }) => {
 	let { category, amount, location, date, paymentType, description } = transaction;
 	const cardClass = categoryColorMap[category];
 
@@ -26,7 +26,7 @@ const Card = ({ transaction }) => {
 	const formattedDate = formatDate(date);
 
 	const [deleteTransaction, { loading }] = useMutation(DELETE_TRANSACTION, {
-		refetchQueries: ["GetTransactions"],
+		refetchQueries: ["GetTransactions", "GetTransactionStatistics"],
 	});
 
 	const handleDelete = async () => {
@@ -70,11 +70,7 @@ const Card = ({ transaction }) => {
 				</p>
 				<div className='flex justify-between items-center'>
 					<p className='text-xs text-black font-bold'>{formattedDate}</p>
-					<img
-						src={"https://tecdn.b-cdn.net/img/new/avatars/2.webp"}
-						className='h-8 w-8 border rounded-full'
-						alt=''
-					/>
+					<img src={authUser?.profilePicture} className='h-8 w-8 border rounded-full' alt='' />
 				</div>
 			</div>
 		</div>
--- frontend/src/components/Cards.jsx
+++ frontend/src/components/Cards.jsx
@@ -1,6 +1,7 @@
 import { useQuery } from "@apollo/client";
 import Card from "./Card";
 import {  GET_TRANSACTIONS } from "../graphql/queries/transaction.query";
+import { GET_AUTHENTICATED_USER } from "../graphql/queries/user.query";
 
 
 
@@ -8,6 +9,7 @@ import {  GET_TRANSACTIONS } from "../graphql/queries/transaction.query";
 const Cards = () => {
 
 	const { data, loading, error} = useQuery(GET_TRANSACTIONS);
+	const { data: authUser } = useQuery(GET_AUTHENTICATED_USER);
      
 	console.log("cards:", data);
 
@@ -16,7 +18,9 @@ const Cards = () => {
 			<p className='text-5xl font-bold text-center my-10'>History</p>
 			<div className='w-full grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 justify-start mb-20'>
 			{!loading &&
-					data.transactions.map((transaction) => <Card key={transaction._id} transaction={transaction} />)}
+					data.transactions.map((transaction) => (
+						<Card key={transaction._id} transaction={transaction} authUser={authUser.authUser} />
+					))}
 			</div>
 			{!loading && data?.transactions?.length === 0 && (
 				<p className='text-2xl font-bold text-center w-full'>No transaction history found.</p>
